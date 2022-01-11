e3a52e0f89b7a6edf4bf731612266488b40e1898
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-01-11 20:06:10 +0100

    create component Product

---
+++ kortings-app/src/Product.js
@@ -0,0 +1,41 @@
+function Product(props) {
+  if(props.item['shop'] === 'AH') {
+    return (
+      <div className="flex-item shop-ah">
+        <span className="product-name">
+          {props.item['product']}
+          <span className="product-info"> {props.item['productInfo']}</span>
+        </span>
+        <span className="product-price">{props.item['price']}</span>   
+        <span className="product-deal">{props.item['deal']}</span>
+   
+      </div>
+    )
+  }
+  else
+    return (
+      <div className="flex-item shop-jumbo">
+        <span className="product-name">
+          {props.item['product']}
+          <span className="product-info"> {props.item['productInfo']}</span>
+        </span>
+        <span className="product-price">{props.item['price']}</span> 
+        <span className="product-deal">{props.item['deal']}</span>
+     
+      </div>
+    )
+
+
+// {
+//   "product": "Campina en Valess - bij 12 euro gratis bezorging",
+//   "productInfo": "hele week",
+//   "deal": "gratis bezorging bij 12 euro",
+//   "price": 0,
+//   "dateStart": "2022-01-10",
+//   "dateEnd": "2022-01-16",
+//   "link": "https://ah.nl/bonus/groep/FREE0222_E?week=2",
+//   "shop": "AH"
+// }
+}
+
+export default Product;
--- parse_offers/__main__.py
+++ parse_offers/__main__.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     albertheijn
     jumbo
 
-    shutil.move('offer-ah.json', 'kortings-app/public/offer-ah.json')
-    shutil.move('offer-jumbo.json', 'kortings-app/public/offer-jumbo.json')
+    shutil.move('offer-ah.json', 'kortings-app/src/offer-ah.json')
+    shutil.move('offer-jumbo.json', 'kortings-app/src/offer-jumbo.json')
