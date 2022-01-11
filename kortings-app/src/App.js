153a087644e9db4824c2cf620f0cad77ea02f863
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-01-11 20:05:57 +0100

    read json offers

--- kortings-app/src/App.js
+++ kortings-app/src/App.js
@@ -1,40 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import offerAh from './offer-ah.json';
+import offerJumbo from './offer-jumbo.json';
+import Product from './Product';
 
 function App() {
   return (
     <div className="App">
+      <div className="info">Er zijn zoveel aanbiedings</div>
       <div className="flex-container">
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-        <div className="flex-item">brood bla bla</div>
-
+      {offerAh.map(function(name, index){
+        return <Product key={index} item={name}/>;
+      })}
       </div>
     </div>
   );
