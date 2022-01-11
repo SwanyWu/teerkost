6c054e8ae7925c1eb7931afc67c2a6ddd821ea40
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-01-11 22:43:06 +0100

    add header

--- kortings-app/src/App.js
+++ kortings-app/src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import Offers from './offers.json';
 import Product from './Product';
 
+
 function App() {
+
+  // const aantalAh = Offers.filter().length;
+
   return (
     <div className="App">
-      <div className="info">Er zijn zoveel aanbiedings</div>
+      <div className="filter-button">FILTEREN</div>
+      <div className="info">
+        Er zijn 120 Albert Heijn en 115 Jumbo aanbieding. 
+      </div>
       <div className="flex-container">
       {Offers.map(function(name, index){
         return <Product key={index} item={name}/>;
