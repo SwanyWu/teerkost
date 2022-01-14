069e897b85f34feaa798400b4aa82cbc66e29410
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-01-14 12:09:02 +0100

    clean up code

--- kortings-app/src/Product.js
+++ kortings-app/src/Product.js
@@ -10,7 +10,6 @@ function Product(props) {
         <span className='product-date-end'>{props.item['dateEnd']}</span>
         <span className="product-price">{props.item['price']}</span>   
         <span className="product-deal">{props.item['deal']}</span>
-   
       </div>
     )
   }
@@ -25,21 +24,8 @@ function Product(props) {
         <span className='product-date-end'>{props.item['dateEnd']}</span>
         <span className="product-price">{props.item['price']}</span> 
         <span className="product-deal">{props.item['deal']}</span>
-     
       </div>
     )
-
-
-// {
-//   "product": "Campina en Valess - bij 12 euro gratis bezorging",
-//   "productInfo": "hele week",
-//   "deal": "gratis bezorging bij 12 euro",
-//   "price": 0,
-//   "dateStart": "2022-01-10",
-//   "dateEnd": "2022-01-16",
-//   "link": "https://ah.nl/bonus/groep/FREE0222_E?week=2",
-//   "shop": "AH"
-// }
 }
 
 export default Product;
