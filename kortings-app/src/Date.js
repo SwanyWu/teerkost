4f826eac333028341950c46c73e8022f5f8a4d20
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-01-31 20:07:10 +0100

    stop showing nan

--- kortings-app/src/Date.js
+++ kortings-app/src/Date.js
@@ -1,17 +1,21 @@
 function Date(props) {
 
-    const datestamp = props.dateEnd.split("-");
-    var year = datestamp[0]
-    var day = datestamp[2]
-
-    var allMonths = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
-    var month = allMonths[datestamp[1]-1]
-
-    return (
-          <span className="product-date-end">{" " + parseInt(day, 10) + " " + month}</span>
-    )
-        
+    if(props.dateEnd !== "") {
+        const datestamp = props.dateEnd.split("-");
+        var day = datestamp[2]
+    
+        var allMonths = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
+        var month = allMonths[datestamp[1]-1]
+    
+        return (
+              <span className="product-date-end">{" " + parseInt(day, 10) + " " + month}</span>
+        )
+    }
+    else {
+        return (
+            ""
+        )
+    }   
 }
 
 export default Date;
-  
