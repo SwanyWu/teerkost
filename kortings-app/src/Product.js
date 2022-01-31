5612c2dfb9f214db162900e99928b75d42c07d42
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-01-31 20:07:34 +0100

    add shop label

--- kortings-app/src/Product.js
+++ kortings-app/src/Product.js
@@ -2,28 +2,14 @@ import Date from './Date';
 import Price from './Price';
 
 function Product(props) {
-  if(props.item['shop'] === 'AH') {
     return (
-      <div className="flex-item shop-ah">
+      <div className="flex-item">
         <span className="product-image"><img alt={props.item['product']} src={props.item['image']}/></span>
         <span className="product-name">
           {props.item['product']}
           <span className="product-info"> {props.item['productInfo']}</span>
         </span>
-        <Date dateEnd={props.item['dateEnd']} />
-        <Price newPrice={props.item['price']} />
-        <span className="product-deal">{props.item['deal']}</span>
-      </div>
-    )
-  }
-  else
-    return (
-      <div className="flex-item shop-jumbo">
-        <span className="product-image"><img alt={props.item['product']} src={props.item['image']}/></span>
-        <span className="product-name">
-          {props.item['product']}
-          <span className="product-info"> {props.item['productInfo']}</span>
-        </span>
+        <span className={"product-shop " + props.item['shop']}>{props.item['shop']}</span>
         <Date dateEnd={props.item['dateEnd']} />
         <Price newPrice={props.item['price']} />
         <span className="product-deal">{props.item['deal']}</span>
