3482094f4bec26cf36eb0cb20fbc03ae7ee9719c
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-02-16 18:09:11 +0100

    location of pricetag

--- kortings-app/src/Product.js
+++ kortings-app/src/Product.js
@@ -6,13 +6,13 @@ function Product(props) {
       <div className="flex-item">
         <span className={"product-shop " + props.item['shop']}>{props.item['shop']}</span>
         <Date dateEnd={props.item['dateEnd']} />
+        <Price newPrice={props.item['price']} />
         <span className="product-image"><img alt={props.item['product']} src={props.item['image']}/></span>
         <span className="product-name">
           {props.item['product']}
           <span className="product-info"> {props.item['productInfo']}</span>
         </span>
         <span className="product-deal">
-          <Price newPrice={props.item['price']} />
           {props.item['deal']}
         </span>
       </div>
