9384893d44daa8b04e3766037da63b31b637a8bf
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-02-24 21:25:44 +0100

    adds tests for categorize

--- parse_offers/test/test_cleanup.py
+++ parse_offers/test/test_cleanup.py
@@ -4,9 +4,37 @@ from cleanup import categorize
 
 class TestCleanup(unittest.TestCase):
 
-    def test_return_category(self):
-        resultingCategory = categorize.findCategoryForProduct("bier", "bier")
+    def test_return_category_by_title(self):
+        resultingCategory = categorize.findCategoryForProduct("bier water", "een flesje")
         self.assertTrue(resultingCategory == 'bier')
+
+    def test_return_category_by_description(self):
+        resultingCategory = categorize.findCategoryForProduct("een flesje", "water bier")
+        self.assertTrue(resultingCategory == 'bier')
+
+    def test_return_category_by_title_w_minus(self):
+        resultingCategory = categorize.findCategoryForProduct("een flesje", "bier-klein-bier")
+        self.assertTrue(resultingCategory == 'bier')
+
+    def test_return_category_by_description_w_minus(self):
+        resultingCategory = categorize.findCategoryForProduct("flesje-bier-groot", "een flesje")
+        self.assertTrue(resultingCategory == 'bier')
+
+    def test_return_category_w_empty_title(self):
+        resultingCategory = categorize.findCategoryForProduct("", "bier")
+        self.assertTrue(resultingCategory == 'bier')    
+    
+    def test_return_category_w_empty_description(self):
+        resultingCategory = categorize.findCategoryForProduct("bier", "")
+        self.assertTrue(resultingCategory == 'bier')   
+
+    def test_return_empty_w_empty_input(self):
+        resultingCategory = categorize.findCategoryForProduct("", "")
+        self.assertTrue(resultingCategory == '') 
+
+    def test_return_empty(self):
+        resultingCategory = categorize.findCategoryForProduct("niets", "niks")
+        self.assertTrue(resultingCategory == '')          
 
 if __name__ == '__main__':
     unittest.main()
