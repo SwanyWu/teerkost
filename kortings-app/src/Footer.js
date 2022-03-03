d1b19a3968a019724667e913026877a1338925a6
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-03-03 22:14:44 +0100

    change name to teerkost

--- kortings-app/src/Footer.js
+++ kortings-app/src/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
       <footer>
-          <span className="title">Kortings</span>
+          <span className="title">Teerkost</span>
           <span className="sub-title">Altijd de actuele aanbiedingen van de Albert Heijn, Jumbo en Lidl.</span>
       </footer>
     )
