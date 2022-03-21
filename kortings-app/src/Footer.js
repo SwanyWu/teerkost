06b9a8b31423cb53868d1e0ab91d6ec4326c76ff
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-03-21 19:57:08 +0100

    update footer text and links

--- kortings-app/src/Footer.js
+++ kortings-app/src/Footer.js
@@ -1,9 +1,9 @@
 function Footer() {
     return (
       <footer>
-          <span className="title">Teerkost</span>
+          <a href='https://teerkost.nl'><span className="title">Teerkost</span></a>
           <span className="sub-title">
-            Altijd de actuele aanbiedingen van de Albert Heijn, Jumbo en Lidl. 
+            Altijd de actuele aanbiedingen van de Albert Heijn, Jumbo, Lidl en Aldi. 
             <a href="https://github.com/ffyud/teerkost" className="source-link">Open source op Github</a>
           </span>
       </footer>
