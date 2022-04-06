ed417c0237df385fff77be1335698b8f4d56abab
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-06 16:31:33 +0200

    add class to kinds of deal

--- kortings-app/src/Deal.js
+++ kortings-app/src/Deal.js
@@ -2,11 +2,21 @@ function Deal(props) {
 
     const dealLabel = props.deal;
 
-    return (
+    if(dealLabel === '1+1 gratis' || dealLabel === '50% korting') {
+      return (
+        <span className="product-deal on-fire">
+          {dealLabel}
+        </span>    
+    )
+    }
+    else {
+      return (
         <span className="product-deal">
           {dealLabel}
         </span>    
     )
+    }
+
 }
 
 export default Deal;
