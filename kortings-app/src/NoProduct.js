ec6b6a8634d90fade0769d3ea480705495b03465
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-08 10:26:00 +0200

    wrap in div

--- kortings-app/src/NoProduct.js
+++ kortings-app/src/NoProduct.js
@@ -1,9 +1,11 @@
 
 function NoProduct() {
     return (
-      <div className="no-offers">
-        <div className='notify-wrap'>
-          Geen korting gevonden met deze filter.
+      <div className="app-wrap">
+        <div className="no-offers">
+          <div className='notify-wrap'>
+            Geen korting gevonden met deze filter.
+          </div>
         </div>
       </div>
     )
