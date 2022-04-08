76d3030521a960d7bb9a899d8dd34559b86b1e72
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-08 15:54:17 +0200

    adds donate banner

--- kortings-app/src/Footer.js
+++ kortings-app/src/Footer.js
@@ -7,6 +7,9 @@ function Footer() {
             Altijd de actuele aanbiedingen van de Albert Heijn, Jumbo, Lidl en Aldi. 
             </span>
           </span>
+          <a target="_blank" rel="noreferrer" href="https://www.paypal.com/donate/?business=27RC7XCK9VHH4&no_recurring=0&currency_code=EUR">
+            <span className="donate"><i className="wave">👋</i> Dit is een hobbyproject, een <i>donatie</i> is welkom!</span>
+          </a>
           <a href="https://github.com/ffyud/teerkost" className="source-link"><i class="ri-github-fill"></i></a>
       </footer>
     )
