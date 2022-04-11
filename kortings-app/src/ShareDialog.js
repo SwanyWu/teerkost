55136b6fb23986d374594dca2c4c2ab9aaa4383d
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-11 14:52:39 +0200

    adds intent links for socials

--- kortings-app/src/ShareDialog.js
+++ kortings-app/src/ShareDialog.js
@@ -1,5 +1,8 @@
 function ShareDialog(props) {
   
+  var shareText = "Altijd de actuele aanbiedingen van de Albert Heijn, Jumbo, Lidl en Aldi."
+  var shareUrl = "https://teerkost.nl"
+
   const copyMe = () => {
     var linkElement = document.querySelector(".url-share input");
     navigator.clipboard.writeText(linkElement.value);
@@ -15,21 +18,21 @@ function ShareDialog(props) {
               <span onClick={copyMe} className="url-copy">kopieer</span>
           </div>
           <div className="socials">
-          <a className="whatsapp" href="https://api.whatsapp.com/send?text=https://teerkost.nl" >
+          <a className="whatsapp" href={"https://api.whatsapp.com/send?text="+shareUrl+""}>
               <i class="ri-whatsapp-fill"></i>
           </a>
-          <a className="telegram" href="https://telegram.me/share/url?url=https://teerkost.nl&text=">
+          <a className="telegram" href={"https://telegram.me/share/url?url="+shareUrl+"&text="+shareText+""}>
               <i class="ri-telegram-fill"></i>
           </a>
-          {/* <a className="twitter" href="">
+          <a className="twitter" href={"https://twitter.com/intent/tweet?url="+shareUrl+"&text="+shareText+""}>
           <i class="ri-twitter-fill"></i>
           </a>
-          <a className="reddit" href="">
+          <a className="reddit" href={"https://reddit.com/submit?url="+shareUrl+"&title=Teerkost"}>
               <i class="ri-reddit-fill"></i>
           </a>
-          <a className="mail" href="">
-              <i class="ri-mail-fill"></i>
-          </a>     */}
+          <a className="linkedin" href={"https://www.linkedin.com/sharing/share-offsite/?url="+shareUrl+""}>
+            <i class="ri-linkedin-box-fill"></i>
+          </a>
           </div>
       </div>
       </div>
