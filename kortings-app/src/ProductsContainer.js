acc647ad02f253b9cf5cecb70eb0dd8054ef6e2a
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-12 16:53:32 +0200

    adds length state and more button

--- kortings-app/src/ProductsContainer.js
+++ kortings-app/src/ProductsContainer.js
@@ -1,12 +1,28 @@
+import React, {useState, useEffect} from "react";
 import NoProduct from "./NoProduct";
 import ProductCard from "./ProductCard";
 
 function ProductsContainer(props) {
+
+    const [totalLength, setTotalLength] = useState(props.selectedOffers.length)
+    const [listLength, setListLength] = useState(100)
+
+    useEffect(() => {
+      setTotalLength(props.selectedOffers.length)
+    })
+
+    const setNewLimit = () => {
+      setListLength(listLength + 100)
+    }
+
     return (
         <main className="flex-container">
-        { props.selectedOffers.length > 0 ? props.selectedOffers.map(function(name, index){
+        { totalLength > 0 ? props.selectedOffers.slice(0,listLength).map(function(name, index){
           return <ProductCard key={index} item={name}/>;
         }) : <NoProduct /> }
+        { totalLength > 100 && listLength < totalLength ? 
+          <div onClick={setNewLimit} className="show-more">
+          <span>Toon meer</span></div> : <div></div>}
         </main>
     )
 }
