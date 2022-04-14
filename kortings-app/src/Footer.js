26ef99dc9a4899cf1b7c82b91ea0e43d2b14b0ca
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-14 18:00:27 +0200

    remove source link

--- kortings-app/src/Footer.js
+++ kortings-app/src/Footer.js
@@ -10,7 +10,6 @@ function Footer() {
           <a target="_blank" rel="noreferrer" href="https://www.paypal.com/donate/?business=27RC7XCK9VHH4&no_recurring=0&currency_code=EUR">
             <span className="donate"><i className="wave">👋</i> Dit is een hobbyproject, een <i>donatie</i> is welkom!</span>
           </a>
-          <a href="https://github.com/ffyud/teerkost" className="source-link"><i class="ri-github-fill"></i></a>
       </footer>
     )
 }
