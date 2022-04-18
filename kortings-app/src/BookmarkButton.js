b6642d347bd86c496677901766c0c5180e6b0125
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-18 08:25:58 +0200

    adds bookmark button component

---
+++ kortings-app/src/BookmarkButton.js
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import localForage from 'localforage'
+import React, {useState, useEffect } from "react";
+
+function BookmarkButton(props) {
+
+    const [bookmarkCount, setBookmarkCount] = useState(props.bookmarkCount);  
+
+    const amountOfBookmarks = () => {
+        localForage.getItem('teerkost-bookmarks').then(function (value) {
+            var amount = 0
+            if(value !== null) {
+                amount = value.length
+            }
+            setBookmarkCount(amount)
+        })
+    } 
+    
+    useEffect(() => {
+        amountOfBookmarks()
+    },[])
+
+    return (
+        <div className="button-cell">
+        <Link to="/bewaard"><div className="button personal-list-button">
+            <i class="ri-bookmark-line"></i> 
+            <div className="personal-list-button-tag">{bookmarkCount}</div>
+        </div></Link>
+        </div>
+    )
+  }
+  
+  export default BookmarkButton;
+    
