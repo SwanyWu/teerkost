6b268be34937a68f693c0e40d2f5ea8ab0ad5fb2
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-18 08:26:10 +0200

    adds no bookmarks component

---
+++ kortings-app/src/NoBookmarks.js
@@ -0,0 +1,14 @@
+
+function NoBookmarks() {
+    return (
+      <div className="app-wrap">
+        <div className="no-offers">
+          <div className='notify-wrap'>
+            Geen bewaarde kortingen gevonden.
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default NoBookmarks;
