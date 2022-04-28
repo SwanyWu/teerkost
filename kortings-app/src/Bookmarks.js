a5753fe9a3e046b4cfba7dbae38a23c6cbf15f73
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-04-28 21:05:44 +0200

    clean up not-existing offers

--- kortings-app/src/Bookmarks.js
+++ kortings-app/src/Bookmarks.js
@@ -15,13 +15,17 @@ function Bookmarks(props) {
 
   const [selectedOffers, setSelectedOffers] = useState([]);  
   const [bookmarkCount, setBookmarkCount] = useState(0);
+  const [oldBookmarksExist, setOldBookmarksExist] = useState(false);
+
 
   useEffect(() => {
     var bookmarkedIds = undefined;
+    
     localForage.getItem('teerkost-bookmarks').then(function (value) {
       if(value !== null) { // if something is present in browserstorage
         bookmarkedIds = value
         updateOfferListById(bookmarkedIds)
+        checkIfOffersExist(bookmarkedIds)
         setBookmarkCount(bookmarkedIds.length)
       } else {
         console.info("Er staan geen bookmarks in deze browser opgeslagen.")
@@ -32,7 +36,6 @@ function Bookmarks(props) {
   }, [])
 
   const updateOfferListById = (bookmarkIdList) => {
-
     var productIdFilter = object => object.productId === null;
 
     if(bookmarkIdList !== null) {
@@ -51,6 +54,38 @@ function Bookmarks(props) {
     console.log("Aantal aanbiedingen na filter: " + filtered.length)
   }
 
+  const checkIfOffersExist = (bookmarkedIds) => {
+    var i = 0;
+    bookmarkedIds.forEach((productId) => {
+      var searchOffersResult = Offers.find(object => object.productId === productId);
+      if(searchOffersResult === undefined) {
+        console.log("Bookmark met het productId " + productId + " niet meer gevonden.")
+        i = i + 1;
+
+        localForage.getItem('teerkost-bookmarks').then(function (value) {
+          if(value !== null) { // if something is present in browserstorage
+            var allBookmarks = value;
+            allBookmarks = allBookmarks.filter(element => element !== productId) // remove from existing array
+            localForage.setItem('teerkost-bookmarks', allBookmarks).then(function(value) {
+              console.log(productId + " is niet meer actueel en wordt verwijdert uit de bookmarks.")
+              setOldBookmarksExist(true)
+            }).catch(function (err) {
+              console.log("Iets misgegaan bij het opslaan van bookmarks: " + err)
+            })
+          }
+        });
+      }
+    }); 
+  }
+
+  const verlopenMelding = (exist) => {
+    if(exist === false) {
+      return "Er zijn niet actuele aanbiedingen verwijdert."
+    } else {
+      return ""
+    }
+  }
+
   return (
     <div className="app-wrap">
         <a className="title-sober" href='https://teerkost.nl'><span>Teerkost</span></a>
@@ -63,10 +98,10 @@ function Bookmarks(props) {
             <div>
               <header className="filter">
                 <div className="filter-wrap">
-                  <span className="bookmark-info"><i class="ri-bookmark-line"></i><span>{bookmarkCount} bewaarde aanbieding{bookmarkCount !== 1 ? "en":""}</span></span>
-                  <div className="bookmark-options">          
-                    {/* <ShareDialog /> */}
-                  </div>
+                  <span className="bookmark-info">
+                    Bewaarde aanbiedingen
+                  </span>
+                  {/* <span className="bookmark-old-info">{verlopenMelding(oldBookmarksExist)}</span> */}
                 </div>
               </header>
               <Suspense fallback={<div className="even-geduld"><div className="notify-wrap"><span className='dot-dot-dot'></span></div></div>}>
