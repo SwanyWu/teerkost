187295bee95466f995e571a11ca9972df91cac38
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-05-06 19:58:54 +0200

    adds buttons, update title

--- kortings-app/src/pages/ListProduct.js
+++ kortings-app/src/pages/ListProduct.js
@@ -4,6 +4,8 @@ import Offers from '../offers.json';
 import ShareDialog from "../components/ShareDialog";
 import BookmarkButton from "../BookmarkButton";
 import { categoryList } from "../Categories";
+import GoHomeButton from "../GoHomeButton";
+import ShareButton from "../ShareButton";
 
 const ProductsContainer = React.lazy(() => import ('../ProductsContainer'));
 
@@ -28,26 +30,34 @@ function Main(props) {
     window.scrollTo(0, 0);
   });
 
+  const updatePageTitle = () => {
+    document.title = "gedeelde lijst - Teerkost"
+  }
+
   const [selectedOffers, setSelectedOffers] = useState(Offers);
   const [productIdsList, setProductIdsList] = useState([]);  
 
   useEffect(() => {
     createProductIdsList(id)
+    updatePageTitle()
   }, [])
 
   return (
     <div className="app-wrap">
+        <ShareDialog buttonText="deel lijst" infoText="Deel een link naar deze lijst met aanbiedingen." />
         <div className="bottom-buttons">
+          <GoHomeButton />
           <BookmarkButton />
-          <ShareDialog buttonText="deel lijst" infoText="Deel een link naar deze lijst met aanbiedingen." />
+          <ShareButton buttonText="deel lijst" infoText="Deel een link naar deze lijst met aanbiedingen."/>
         </div>
         <a className="title-sober" href='https://teerkost.nl'><span>Teerkost</span></a>
         <header className="filter">
-            <div id="filter-dialog">
-            <div className="filter-wrap">
-            </div>
-            </div>
-        </header>
+                <div className="filter-wrap">
+                  <span className="bookmark-info">
+                    Gedeelde lijst
+                  </span>
+                </div>
+              </header>
         <Suspense fallback={<div className="even-geduld"><div className="notify-wrap"><span className='dot-dot-dot'></span></div></div>}>
             <ProductsContainer selectedOffers={selectedOffers}/>
         </Suspense>
