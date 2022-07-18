758d8742410b8920d13cfc66391baf29dfd3f696
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-07-18 17:25:48 +0200

    fix donate link position

--- kortings-app/src/components/Footer.js
+++ kortings-app/src/components/Footer.js
@@ -7,8 +7,8 @@ function Footer() {
             Altijd de actuele aanbiedingen van supermarkten.
             </span>
           </span>
-          <a target="_blank" rel="noreferrer" href="https://www.paypal.com/donate/?business=27RC7XCK9VHH4&no_recurring=0&currency_code=EUR">
-            <span className="donate">Dit is een hobbyproject, een <i>donatie</i> is welkom!</span>
+          <a className="donate" target="_blank" rel="noreferrer" href="https://www.paypal.com/donate/?business=27RC7XCK9VHH4&no_recurring=0&currency_code=EUR">
+            <span>Dit is een hobbyproject, een <i>donatie</i> is welkom!</span>
           </a>
       </footer>
     )
