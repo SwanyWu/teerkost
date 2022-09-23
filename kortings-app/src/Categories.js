b1f366679e85720543797fb59b118b24e5a3793c
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-09-23 11:12:45 +0200

    add category 'sap'

--- kortings-app/src/Categories.js
+++ kortings-app/src/Categories.js
@@ -2,7 +2,7 @@ export const categoryList = [
     'groente', 'vlees', 
     'vis','fruit', 'vegan', 'bier', 'zuivel', 'kant-en-klaar', 'wijn',
     'aardappel','brood','kaas','noten','koffie',
-    'thee','beleg','ontbijt','frisdrank','pasta',
+    'thee','beleg','ontbijt','sap','frisdrank','pasta',
     'koek','ijs','chocola','chips','snoep', 'gebak',
     'verzorging','huishouden', 'tuin', 'dier', 'geen-categorie'
   ]
