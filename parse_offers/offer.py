79eb8e8455e2bdfd3758a9a94d6b55a7829c9f1b
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-10-16 16:46:07 +0200

    add offer

---
+++ parse_offers/offer.py
@@ -0,0 +1,13 @@
+offer = {
+    "productId": "",
+    "product":"", 
+    "productInfo":"", 
+    "category":"", 
+    "image":"", 
+    "deal":"",
+    "price": float(0), 
+    "dateStart":"", 
+    "dateEnd":"", 
+    "link": "", 
+    "shop":""
+ }
